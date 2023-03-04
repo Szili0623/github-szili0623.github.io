4ee9a272ac1e40fc5126dd7278a6d35ad273acc2
Szili0623 <szili0623@gmail.com>
2023-03-04 17:42:56 +0100

    To-do

--- to-do-list-test/scripts.js
+++ to-do-list-test/scripts.js
@@ -2,3 +2,19 @@ $("#todoButton").click(() => {
   let todo = $("#todoInput").val()
   $("body").append(`<p>${todo}</p>`)
 })
+
+let buttonCounter = 1
+$("#moreButton").click(() => {
+  $("html").append(`<div class="container${buttonCounter}"></div>`)
+  $(`.container${buttonCounter}`).append(`<button id="buttonNumber${buttonCounter}">${buttonCounter}</button>`)
+  buttonCounter = buttonCounter + 1
+  })
+
+let gomb = ($("html").click(() => {
+  $("").html(event.target.id)
+  console.log(event.target.id)
+}))
+
+$("button").click(() => {
+  $("#gomb").css("background", "yellow")
+})
--- to-do-list/scripts.js
+++ to-do-list/scripts.js
@@ -1,5 +1,17 @@
+let todoCount = 1
 $("#todoAdd").click(() => {
   let todotask = $("#inputBox").val()
-  $(".todoList").append(`<p>${todotask}</p>`)
-  $(".todoList").append
+  $(".todoList").append(`<div class="listItem${todoCount} listItems"></div>`)
+  $(`.listItem${todoCount}`).append(`<p class="listContent ${todoCount}">${todotask}</p>`)
+  $(`.listItem${todoCount}`).append(`<div class="listItemRight${todoCount} listItemRights"></div>`)
+  $(`.listItemRight${todoCount}`).append(`<i class="fa-solid fa-trash fa-2x trashIcons ${todoCount}""></i>`)
+  $(`.listItemRight${todoCount}`).append(`<span class="todoCheck"><i class="fa-regular fa-circle-check fa-2x todoChecks ${todoCount}"></i></span>`)
+  $("#inputBox").val("")
+  todoCount = todoCount + 1
+  
+  $("span").click(() => {
+
+    console.log("ball")
+  })
+
   })
